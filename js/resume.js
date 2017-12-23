bec0a2c058e48d7a3064c7252bfbb22c2875cf0d
Chandrasekar Sivaraman <csivaraman1808@gmail.com>
2017-12-22 18:29:06 -0800

    added google map to contact

--- js/resume.js
+++ js/resume.js
@@ -27,4 +27,27 @@
 
   $('[data-toggle="tooltip"]').tooltip();
 
+  function init_map() {
+    var var_location = new google.maps.LatLng(34.052235, -118.243683);
+
+    var var_mapoptions = {
+      center: var_location,
+      zoom: 10
+    };
+
+    var var_marker = new google.maps.Marker({
+      position: var_location,
+      map: var_map,
+      title: "Los Angeles"
+    });
+
+    var var_map = new google.maps.Map(document.getElementById("map"),
+    var_mapoptions);
+
+    var_marker.setMap(var_map);
+
+  }
+
+  google.maps.event.addDomListener(window, 'load', init_map);
+
 })(jQuery); // End of use strict
